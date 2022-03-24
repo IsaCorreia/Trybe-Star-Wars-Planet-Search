d3c046890be433e29d5fa74342bc1f7c3a3b731b
Isabela Correia <isabela.correiasilva00@gmail.com>
2022-03-24 18:39:32 -0300

    FilterBar.js - Implementando inputs para os filtros numericos (coluna, operador e numero), definindo valor de inicio para o input de numero

--- src/components/FilterBar.js
+++ src/components/FilterBar.js
@@ -2,7 +2,27 @@ import React, { useContext } from 'react';
 import MyContext from '../context/myContext';
 
 function FilterBar() {
-  const { onInputChange } = useContext(MyContext);
+  const {
+    planets,
+    onInputChange,
+    applyFilters,
+    filterInput: { filterByNumValue },
+  } = useContext(MyContext);
+
+  const columnOptionGen = () => {
+    if (planets.length) {
+      const desiredHeads = [
+        'population',
+        'orbital_period',
+        'diameter',
+        'rotation_period',
+        'surface_water',
+      ];
+      return desiredHeads.map((stat, index) => (
+        <option key={ index }>{stat}</option>
+      ));
+    }
+  };
   return (
     <>
       <h3>Filtros:</h3>
@@ -13,6 +33,39 @@ function FilterBar() {
         placeholder="Nome do Planeta"
         onChange={ onInputChange }
       />
+      <label htmlFor="filterByNumColumn">
+        Coluna
+        <select
+          id="filterByNumColumn"
+          data-testid="column-filter"
+          onChange={ onInputChange }
+        >
+          {columnOptionGen()}
+        </select>
+      </label>
+
+      <label htmlFor="filterByNumOperator">
+        Operador
+        <select
+          id="filterByNumOperator"
+          data-testid="comparison-filter"
+          onChange={ onInputChange }
+        >
+          <option>maior que</option>
+          <option>menor que</option>
+          <option>igual a</option>
+        </select>
+      </label>
+      <input
+        type="number"
+        id="filterByNumValue"
+        data-testid="value-filter"
+        onChange={ onInputChange }
+        value={ filterByNumValue }
+      />
+      <button type="button" data-testid="button-filter" onClick={ applyFilters }>
+        Filtrar
+      </button>
     </>
   );
 }
