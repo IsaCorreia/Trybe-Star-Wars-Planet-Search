4d2767e9543191f42e8df041a7463d78ba44a916
Isabela Correia <isabela.correiasilva00@gmail.com>
2022-03-25 11:53:06 -0300

    Table.js - Retrabalhando a funçao de renderizar tabela para aceitar o array filtrado

--- src/components/Table.js
+++ src/components/Table.js
@@ -2,53 +2,59 @@ import React, { useContext } from 'react';
 import MyContext from '../context/myContext';
 
 function Table() {
-  const { planets, filterInput: { filterByName } } = useContext(MyContext);
+  const {
+    planets,
+    filterInput: { filterByName },
+    filteredPlanets,
+  } = useContext(MyContext);
 
   const tableHeadGen = () => {
     if (planets.length) {
       const tableHeadText = Object.keys(planets[0]);
       return tableHeadText
         .filter((header) => header !== 'residents')
-        .map((header, index) => <th key={ index }>{ header }</th>);
+        .map((header, index) => <th key={ index }>{header}</th>);
     }
   };
 
   const tableContentGen = () => {
-    if (planets.length) {
-      return planets
+    if (filteredPlanets.length) {
+      return filteredPlanets
         .filter((planet) => planet.name.toUpperCase()
           .includes(filterByName.toUpperCase()))
         .map(
-          ({
-            name,
-            rotation_period: rotation,
-            orbital_period: orbital,
-            diameter,
-            climate,
-            gravity,
-            terrain,
-            surface_water: surface,
-            population,
-            films,
-            created,
-            edited,
-            url,
-          },
-          index) => (
+          (
+            {
+              name,
+              rotation_period: rotation,
+              orbital_period: orbital,
+              diameter,
+              climate,
+              gravity,
+              terrain,
+              surface_water: surface,
+              population,
+              films,
+              created,
+              edited,
+              url,
+            },
+            index,
+          ) => (
             <tr key={ index }>
               <td key={ name + index }>{name}</td>
               <td key={ rotation + index }>{rotation}</td>
               <td key={ orbital + index }>{orbital}</td>
-              <td key={ diameter + index }>{ diameter }</td>
-              <td key={ climate + index }>{ climate }</td>
-              <td key={ gravity + index }>{ gravity }</td>
-              <td key={ terrain + index }>{ terrain }</td>
-              <td key={ surface + index }>{ surface }</td>
-              <td key={ population + index }>{ population }</td>
-              <td key={ films + index }>{ films }</td>
-              <td key={ created + index }>{ created }</td>
-              <td key={ edited + index }>{ edited }</td>
-              <td key={ url + index }>{ url }</td>
+              <td key={ diameter + index }>{diameter}</td>
+              <td key={ climate + index }>{climate}</td>
+              <td key={ gravity + index }>{gravity}</td>
+              <td key={ terrain + index }>{terrain}</td>
+              <td key={ surface + index }>{surface}</td>
+              <td key={ population + index }>{population}</td>
+              <td key={ films + index }>{films}</td>
+              <td key={ created + index }>{created}</td>
+              <td key={ edited + index }>{edited}</td>
+              <td key={ url + index }>{url}</td>
             </tr>
           ),
         );
@@ -60,7 +66,9 @@ function Table() {
       <thead>
         <tr>{tableHeadGen()}</tr>
       </thead>
-      <tbody>{tableContentGen()}</tbody>
+      <tbody>
+        {tableContentGen()}
+      </tbody>
     </table>
   );
 }
