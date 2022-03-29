0cedb1da5587418d83dbbecccf82897ca34f78e7
Isabela Correia <isabela.correiasilva00@gmail.com>
2022-03-29 19:46:57 -0300

    Table - refatorando a funçao tableCOntentGen, aplicando o filtro numerico

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,63 +1,64 @@
-import React, { useContext } from 'react';
-import MyContext from '../context/myContext';
+import React, { useContext } from "react";
+import MyContext from "../context/myContext";
 
 function Table() {
   const {
     planets,
+    applyNumFilter,
     filterInput: { filterByName },
-    filteredPlanets,
   } = useContext(MyContext);
 
   const tableHeadGen = () => {
     if (planets.length) {
       const tableHeadText = Object.keys(planets[0]);
       return tableHeadText
-        .filter((header) => header !== 'residents')
-        .map((header, index) => <th key={ index }>{header}</th>);
+        .filter((header) => header !== "residents")
+        .map((header, index) => <th key={index}>{header}</th>);
     }
   };
 
   const tableContentGen = () => {
-    if (filteredPlanets.length) {
-      return filteredPlanets
-        .filter((planet) => planet.name.toUpperCase()
-          .includes(filterByName.toUpperCase()))
-        .map(
-          (
-            {
-              name,
-              rotation_period: rotation,
-              orbital_period: orbital,
-              diameter,
-              climate,
-              gravity,
-              terrain,
-              surface_water: surface,
-              population,
-              films,
-              created,
-              edited,
-              url,
-            },
-            index,
-          ) => (
-            <tr key={ index }>
-              <td key={ name + index }>{name}</td>
-              <td key={ rotation + index }>{rotation}</td>
-              <td key={ orbital + index }>{orbital}</td>
-              <td key={ diameter + index }>{diameter}</td>
-              <td key={ climate + index }>{climate}</td>
-              <td key={ gravity + index }>{gravity}</td>
-              <td key={ terrain + index }>{terrain}</td>
-              <td key={ surface + index }>{surface}</td>
-              <td key={ population + index }>{population}</td>
-              <td key={ films + index }>{films}</td>
-              <td key={ created + index }>{created}</td>
-              <td key={ edited + index }>{edited}</td>
-              <td key={ url + index }>{url}</td>
-            </tr>
-          ),
-        );
+    if (planets.length) {
+      const filtered = planets.filter((planet) =>
+        planet.name.toUpperCase().includes(filterByName.toUpperCase())
+      );
+
+      return applyNumFilter(filtered).map(
+        (
+          {
+            name,
+            rotation_period: rotation,
+            orbital_period: orbital,
+            diameter,
+            climate,
+            gravity,
+            terrain,
+            surface_water: surface,
+            population,
+            films,
+            created,
+            edited,
+            url,
+          },
+          index
+        ) => (
+          <tr key={index}>
+            <td key={name + index}>{name}</td>
+            <td key={rotation + index}>{rotation}</td>
+            <td key={orbital + index}>{orbital}</td>
+            <td key={diameter + index}>{diameter}</td>
+            <td key={climate + index}>{climate}</td>
+            <td key={gravity + index}>{gravity}</td>
+            <td key={terrain + index}>{terrain}</td>
+            <td key={surface + index}>{surface}</td>
+            <td key={population + index}>{population}</td>
+            <td key={films + index}>{films}</td>
+            <td key={created + index}>{created}</td>
+            <td key={edited + index}>{edited}</td>
+            <td key={url + index}>{url}</td>
+          </tr>
+        )
+      );
     }
   };
 
@@ -66,9 +67,7 @@ function Table() {
       <thead>
         <tr>{tableHeadGen()}</tr>
       </thead>
-      <tbody>
-        {tableContentGen()}
-      </tbody>
+      <tbody>{tableContentGen()}</tbody>
     </table>
   );
 }
