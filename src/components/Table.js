12cbd131d5d5e81d4609e96f117a1c7df5738acf
Isabela Correia <isabela.correiasilva00@gmail.com>
2022-03-29 21:20:16 -0300

    Table - Alterando a renderizaçao condicional

--- src/components/Table.js
+++ src/components/Table.js
@@ -18,47 +18,45 @@ function Table() {
   };
 
   const tableContentGen = () => {
-    if (planets.length) {
-      const filtered = planets.filter((planet) => planet.name.toUpperCase()
-        .includes(filterByName.toUpperCase()));
+    const filtered = planets.filter((planet) => planet.name.toUpperCase()
+      .includes(filterByName.toUpperCase()));
 
-      return applyNumFilter(filtered).map(
-        (
-          {
-            name,
-            rotation_period: rotation,
-            orbital_period: orbital,
-            diameter,
-            climate,
-            gravity,
-            terrain,
-            surface_water: surface,
-            population,
-            films,
-            created,
-            edited,
-            url,
-          },
-          index,
-        ) => (
-          <tr key={ index }>
-            <td key={ name + index }>{name}</td>
-            <td key={ rotation + index }>{rotation}</td>
-            <td key={ orbital + index }>{orbital}</td>
-            <td key={ diameter + index }>{diameter}</td>
-            <td key={ climate + index }>{climate}</td>
-            <td key={ gravity + index }>{gravity}</td>
-            <td key={ terrain + index }>{terrain}</td>
-            <td key={ surface + index }>{surface}</td>
-            <td key={ population + index }>{population}</td>
-            <td key={ films + index }>{films}</td>
-            <td key={ created + index }>{created}</td>
-            <td key={ edited + index }>{edited}</td>
-            <td key={ url + index }>{url}</td>
-          </tr>
-        ),
-      );
-    }
+    return applyNumFilter(filtered).map(
+      (
+        {
+          name,
+          rotation_period: rotation,
+          orbital_period: orbital,
+          diameter,
+          climate,
+          gravity,
+          terrain,
+          surface_water: surface,
+          population,
+          films,
+          created,
+          edited,
+          url,
+        },
+        index,
+      ) => (
+        <tr key={ index }>
+          <td key={ name + index }>{name}</td>
+          <td key={ rotation + index }>{rotation}</td>
+          <td key={ orbital + index }>{orbital}</td>
+          <td key={ diameter + index }>{diameter}</td>
+          <td key={ climate + index }>{climate}</td>
+          <td key={ gravity + index }>{gravity}</td>
+          <td key={ terrain + index }>{terrain}</td>
+          <td key={ surface + index }>{surface}</td>
+          <td key={ population + index }>{population}</td>
+          <td key={ films + index }>{films}</td>
+          <td key={ created + index }>{created}</td>
+          <td key={ edited + index }>{edited}</td>
+          <td key={ url + index }>{url}</td>
+        </tr>
+      ),
+    );
   };
 
   return (
@@ -66,7 +64,7 @@ function Table() {
       <thead>
         <tr>{tableHeadGen()}</tr>
       </thead>
-      <tbody>{tableContentGen()}</tbody>
+      <tbody>{planets.length && tableContentGen()}</tbody>
     </table>
   );
 }
